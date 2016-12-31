85391be00a5d60c39fb90aa69181e55b602e54df
Aswin <aswinvk28@gmail.com>
2016-12-31 00:17:05 +0000

    screenshot, demo, styling

--- pages/features/demointerface.tpl.php
+++ pages/features/demointerface.tpl.php
@@ -1,10 +1,15 @@
 <div class="clearfix section-page-file">
     <p>Paper based and hand drawn sketches help in the decision making process that cater for the desired design that needs to be documented. Enscalo manages an ideal workspace 
-    to set the ideas and combine them with technical requirements of the project. The users can switch between workspaces to develop the floor plans for each floor of the required building. *
+        to set the ideas and combine them with technical requirements of the project. The users can switch between workspaces to develop the floor plans for each floor of the required building. *</p>
     <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6">
-        <img src="/assets/style/images/interface.jpeg" alt="Interface" title="Interface" />
+        <div class="img-responsive img-thumbnail img-rounded">
+            <img src="/assets/style/images/interface-v1.jpg" alt="Interface" title="Interface" height="80" width="auto" />
+        </div>
     </div>
     <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6">
-        <video></video>
+        <div class="img-responsive img-thumbnail img-rounded">
+            <img src="/assets/style/images/demo-v1.jpg" alt="Interface" title="Interface" width="auto" height="80" />
+        </div>
     </div>
+    <p class="text-center"><em>A demo version of the prototype software</em></p>
 </div>
