ff9cde4758f1949ef8f6422899455a99180ddca9
Aswin <aswinvk28@gmail.com>
2017-01-04 11:05:26 +0000

    Screenshots added before Video Upload

--- pages/home/showcase.tpl.php
+++ pages/home/showcase.tpl.php
@@ -1,13 +1,16 @@
 <div class="clearfix section-page-file" id="showcase">
-    <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
-        <div class="img-thumbnail bg-img-pan img-screenshot" style="height: 190px">
+    <div class="col-lg-8 col-md-8 col-sm-8 col-xs-12">
+        <div class="img-thumbnail bg-img-pan img-screenshot" style="height: 490px">
             <img src="/assets/style/images/slogan-v1.png" class="img-responsive center-block" />
         </div>
     </div>
-    <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+    <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12">
         <h4 class="heading-bold-home">Floor Plans drawn with ease of use</h4>
         <p class="body-large">A lot of time is in stake to produce an iterative document. 
             We provide a platform to develop your drawings and record your observations to enable ease of use and provide better user experience. 
         </p>
+        <div class="img-thumbnail">
+            <img src="/assets/style/images/live_screenshots/screenshot-printmode-slide.png" class="img-responsive center-block" />
+        </div>
     </div>
 </div>
